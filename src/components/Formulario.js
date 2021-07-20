ebe19959537ffb41faf41b9a56b38b00829197ea
RonnyU <ronnymagdiel@gmail.com>
2021-07-19 21:43:00 -0600

    firts commit

---
+++ src/components/Formulario.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import Error from './Error';
+import shortid from 'shortid';
+import PropTypes from 'prop-types';
+
+const Formulario = ({ guardargasto, guardarCrearGasto }) => {
+  const [nombre, guardarNombre] = useState('');
+  const [cantidad, guardarCantidad] = useState(0);
+  const [error, guardarError] = useState(false);
+
+  const agregarGasto = (e) => {
+    e.preventDefault();
+
+    if (cantidad < 1 || isNaN(cantidad) || nombre.trim() === '') {
+      guardarError(true);
+      return;
+    }
+    guardarError(false);
+
+    const gasto = {
+      nombre,
+      cantidad,
+      id: shortid.generate(),
+    };
+
+    guardargasto(gasto);
+    guardarCrearGasto(true);
+    guardarNombre('');
+    guardarCantidad(0);
+  };
+
+  return (
+    <form onSubmit={agregarGasto}>
+      <h2>Agrega tus gastos aquí</h2>
+
+      {error ? (
+        <Error mensaje='Ambos campos son obligatorios o incorrectos' />
+      ) : null}
+      <div className='campo'>
+        <label>Nombre Gasto</label>
+        <input
+          type='text'
+          className='u-full-width'
+          placeholder='Ej. transporte'
+          value={nombre}
+          onChange={(e) => guardarNombre(e.target.value)}
+        />
+      </div>
+      <div className='campo'>
+        <label>Nombre Gasto</label>
+        <input
+          type='number'
+          className='u-full-width'
+          placeholder='Ej. 300'
+          value={cantidad}
+          onChange={(e) => guardarCantidad(parseInt(e.target.value, 10))}
+        />
+      </div>
+
+      <input
+        type='submit'
+        className='button-primary u-full-width'
+        value='Agregar Gasto'
+      />
+    </form>
+  );
+};
+
+Formulario.prototype = {
+  guardargasto: PropTypes.func.isRequired,
+  guardarCrearGasto: PropTypes.func.isRequired,
+};
+
+export default Formulario;
